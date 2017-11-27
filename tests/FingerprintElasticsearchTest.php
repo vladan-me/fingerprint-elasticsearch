9527d42cb38e33d0f0bed6586f59dba49a253d39
Vladan Djokic <vladan.me@gmail.com>
2017-11-27 20:59:35 +0200

    Fix tests and add additional one for title

--- tests/FingerprintElasticsearchTest.php
+++ tests/FingerprintElasticsearchTest.php
@@ -4,6 +4,7 @@ namespace tests;
 
 use Vladanme\Fingerprint\FingerprintType;
 use Vladanme\FingerprintElasticsearch\ESCity;
+use Vladanme\FingerprintElasticsearch\ESTitle;
 use Vladanme\FingerprintElasticsearch\FingerprintElasticsearch;
 use Vladanme\FingerprintElasticsearch\FingerprintElasticsearchType;
 
@@ -26,7 +27,12 @@ class FingerprintElasticsearchTest extends \PHPUnit_Framework_TestCase
 
     public function testCityRem()
     {
-        $expectedResult = ['a', 'of', 'on'];
+        // First three are coming from extended city specific removals
+        // and 'the' is from English stopwords that make sense for cities.
+        // Even though 'a', 'of' and 'on' are also stopwords they are not
+        // included by default in standard fingerprint algorithm but for
+        // Elasticsearch index it doesn't cost us anything to include all of them.
+        $expectedResult = ['a', 'of', 'on', 'the'];
 
         $ESCity = new ESCity();
 
@@ -62,14 +68,20 @@ class FingerprintElasticsearchTest extends \PHPUnit_Framework_TestCase
             'type'     => 'synonym',
             'synonyms' => [
               'afb => air force base',
+              'bch => beach',
               'cty => city',
+              'e => east',
               'fls => falls',
-                // Notice that it should merge two
-                // or more synonyms into one mapping.
-              'hts,hgts => heights',
+              'ft => fort',
+              'hgts,hts => heights',
               'hls => hills',
+              'mt => mount',
+              'n => north',
+              'st => saint',
+              's => south',
               'vly => valley',
               'vlg => village',
+              'w => west',
             ],
           ],
           'fp_city_rem' => [
@@ -78,34 +90,131 @@ class FingerprintElasticsearchTest extends \PHPUnit_Framework_TestCase
               'a',
               'of',
               'on',
+              'the',
             ],
           ],
         ];
         $expectedProp = [
-            'type'         => 'keyword',
-            'ignore_above' => 256,
-            'fields'       => [
-              'fp'              => [
-                'type'     => 'text',
-                'analyzer' => 'fp_city_analyzer',
-              ],
-              'puncteater'      => [
-                'type'     => 'text',
-                'analyzer' => 'puncteater',
-              ],
-              'puncteater2gram' => [
-                'type'     => 'text',
-                'analyzer' => 'puncteater2gram',
-              ],
-              'puncteater3gram' => [
-                'type'     => 'text',
-                'analyzer' => 'puncteater3gram',
-              ],
+          'type'         => 'keyword',
+          'ignore_above' => 256,
+          'fields'       => [
+            'fp'              => [
+              'type'     => 'text',
+              'analyzer' => 'fp_city_analyzer',
             ],
+            'puncteater'      => [
+              'type'     => 'text',
+              'analyzer' => 'puncteater',
+            ],
+            'puncteater2gram' => [
+              'type'     => 'text',
+              'analyzer' => 'puncteater2gram',
+            ],
+            'puncteater3gram' => [
+              'type'     => 'text',
+              'analyzer' => 'puncteater3gram',
+            ],
+          ],
         ];
 
         $this->assertEquals($expectedAnalyzer, $analyzer);
         $this->assertEquals($expectedFilter, $filter);
         $this->assertEquals($expectedProp, $prop);
+    }
+
+    public function testTitleSynonymsRemovals()
+    {
+        $ESTitle = new ESTitle();
+
+        $filter = $ESTitle->filterES();
+
+        $expectedFilterSynonyms = [
+          'adj => adjunct',
+          'administrato,administr,admin,administrat,administrati,administratio,administra,admi,administ,administration,admini,adminis => administrator',
+          'ast,asst,assistan,assi,assistants,assista,assist,assis => assistant',
+          'assoc,assc => associate',
+          'chair => chairman',
+          'ceo => chief executive officer',
+          'cfo => chief financial officer',
+          'cmo => chief marketing officer',
+          'coo => chief operating officer',
+          'cto => chief technology officer',
+          'clin => clinical',
+          'ctr => control',
+          'directo,directors,direc,dir,dire => director',
+          'educ => education',
+          'engin,engi,eng,enginee,engineers => engineer',
+          'enviro,environment,environ,env,envir => environmental',
+          'exe,execut,executives,execu,exec,executiv,executi => executive',
+          'hum,huma => human',
+          'hr,h r,human res => human resources',
+          'inform,info,informat,infor,informa,informati,informatio,inf,informations => information',
+          'it,i t => information technology',
+          'instr => instructor',
+          'jr => junior',
+          'man,mgr,manag,manage,ma,mngr,mana,managers => manager',
+          'offi,officers,offic,off => officer',
+          'operation,operatio,ope,opera,operat,operati,oper => operations',
+          'owners => owner',
+          'presiden,presid,preside,presi,pres => president',
+          'pro => professional',
+          'rep => representative',
+          'resou,reso,resourc,resour,resource => resources',
+          'sls => sales',
+          'sci => science',
+          'snr,sr => senior',
+          'servi,srvs,srv,servic,service,srvcs,srvc,ser,serv => services',
+          'stf => staff',
+          'supp,supt,spt => support',
+          'system,syste,syst,sys => systems',
+          'technolog,techno,technol,techn,technolo,technologies,tech => technology',
+          'us,usa => united states',
+          'v p,vp => vice president',
+        ];
+        $expectedFilterRemovals = [
+          'united states',
+          'st',
+          'nd',
+          'rd',
+          'th',
+          'first',
+          'second',
+          'third',
+          'fourth',
+          'fifth',
+          'sixth',
+          'seventh',
+          'eighth',
+          'ninth',
+          'grade',
+          'global',
+          'front',
+          'end',
+          'web',
+          'top',
+          'i',
+          'ii',
+          'iii',
+          'iv',
+          'europe',
+          'asia',
+          'pacific',
+          'latin',
+          'canada',
+          'an',
+          'and',
+          'at',
+          'for',
+          'in',
+          'of',
+          'or',
+          'the',
+          'to',
+        ];
+        $filterSynonyms = $filter['fp_title_syn']['synonyms'];
+        $filterRemovals = $filter['fp_title_rem']['stopwords'];
+
+        $this->assertEquals($expectedFilterSynonyms, $filterSynonyms);
+        $this->assertEquals($expectedFilterRemovals, $filterRemovals);
     }
 }
